cd6d6b2667a97d73c663bca45e30836d0845afd3
Rodrigo <sschuckk@gmail.com>
2024-03-28 14:32:29 -0400

    Change file name from MySQLTestTest to MySQLTest.

--- src/test/java/org/schuck/tests/MySQLTest.java
+++ src/test/java/org/schuck/tests/MySQLTest.java
@@ -10,9 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class MySQLTestTest {
+class MySQLTest {
     private Connection connection;
     private Statement dbStatement;
 
